e3875705d9560cee3e3b3c494c35c9347f52d8d1
Roger <evolpirate@gmail.com>
2019-11-05 15:56:16 -0500

    finished using open weather map api to get weather from any city

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
+
+const apiKey = process.env.APIKEY;
 
 // setting view engine
 app.set('view engine', 'ejs');
@@ -16,13 +20,27 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // GET /
 app.get('/', function(req, res){
-    res.render( 'home.ejs', { weather: 'coming soon' } );
+    res.render( 'home.ejs', { weather: null, error: null } );
 });
 
-app.post('/', function(req, res){
-    let weatherNow = "The weather in " + req.body.city + " is 70 C";
-    let err = false;
-    res.render( 'home.ejs', { weather: weatherNow, error: err } );
+
+
+app.post('/', function(req, res){    
+    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + req.body.city + '&units=imperial&APPID='+apiKey;
+
+    request(url , function (error, response, body) {
+        if(error){
+            res.render('home.ejs', { weather: null, error: "Error, please try again"})
+        } else {
+            let weather = JSON.parse(body);
+            if(weather.main == undefined){
+                res.render('home.ejs', { weather: null, error: "Error, please try again"})
+            } else {
+                let weatherNow = "The weather in " + req.body.city + " is " + weather.main.temp + " degrees.";
+                res.render( 'home.ejs', { weather: weatherNow, error: null } );
+            }
+        }
+    }); 
 });
 
 app.listen(3000, function(){
